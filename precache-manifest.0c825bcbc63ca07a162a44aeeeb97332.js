777b461800df432ca731a62ec573374a01ad6470
Shantur Rathore <shantur.rathore@gmail.com>
2020-01-14 16:59:09 +0000

    Publishing TasUI 553a398f84f0858d664d258493725e6115206c81     Branch :master     Commmit: 553a398f84f0858d664d258493725e6115206c81 https://github.com/IOTReboot/TasUI/tree/553a398f84f0858d664d258493725e6115206c81     Local Changes: true
        Local Changes {
      "files": [
        {
          "file": "utils/publish.js",
          "changes": 4,
          "insertions": 2,
          "deletions": 2,
          "binary": false
        }
      ],
      "insertions": 2,
      "deletions": 2,
      "changed": 1
    }

---
+++ precache-manifest.0c825bcbc63ca07a162a44aeeeb97332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68ecdc8ed4fc55d247dcd584e37bc7f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72fbddd02d38eeb6ddf6",
+    "url": "./static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "6cb0401ee94586c4398c",
+    "url": "./static/js/2.10fdf520.chunk.js"
+  },
+  {
+    "revision": "72fbddd02d38eeb6ddf6",
+    "url": "./static/js/main.f9b46f48.chunk.js"
+  },
+  {
+    "revision": "8e535f8e9d7ad097535b",
+    "url": "./static/js/runtime-main.195c3e23.js"
+  }
+]);
